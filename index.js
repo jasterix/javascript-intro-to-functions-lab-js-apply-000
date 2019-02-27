1a9d8e672d56384f68f2b4e1af2610659aaa88a0
Lj C <ljcalixte@gmail.com>
2019-02-27 20:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ function whisper(string) {
 }
 
 function logShout(string) {
+  
+}
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+
+function logShout(string) {
   console.log(logShout)
 }
 
